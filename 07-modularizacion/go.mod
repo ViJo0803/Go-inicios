1841b8b85a81a969a207c625b12d1dd4f5390191
ViJo0803 <vijo967@gmail.com>
2022-02-13 23:46:47 -0500

    modulos externos

--- 07-modularizacion/go.mod
+++ 07-modularizacion/go.mod
@@ -1,3 +1,5 @@
 module paque
 
 go 1.17
+
+require github.com/donvito/hellomod v1.0.1 // indirect
--- 07-modularizacion/mensajes/mensajes.go
+++ 07-modularizacion/mensajes/mensajes.go
@@ -5,3 +5,14 @@ import "fmt"
 func Hola() {
 	fmt.Println("Hola desde el paquete mensaje")
 }
+
+const mensaje = "Hola desde mi constantes"
+
+func funcionPrivada() {
+	fmt.Println("Hola desde la funcion privada")
+}
+
+func Imprimir() {
+	fmt.Println(mensaje)
+	funcionPrivada()
+}
